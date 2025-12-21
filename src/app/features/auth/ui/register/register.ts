f357cdf702bfa162da18d42a90a68fdc2bc41945
IsaevKA <kostik.isaev.97@list.ru>
2025-12-21 21:35:17 +0300

    add phoneNumber validator

--- src/app/features/auth/ui/register/register.ts
+++ src/app/features/auth/ui/register/register.ts
@@ -8,6 +8,7 @@ import { UiLanguageGroup } from '../../../../shared/ui/ui-language-group/ui-lang
 import { TranslateModule } from '@ngx-translate/core';
 import { UiInputPhone } from '../../../../shared/ui/ui-input-phone/ui-input-phone';
 import { UiInputPassword } from '../../../../shared/ui/ui-input-password/ui-input-password';
+import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
 
 @Component({
   selector: 'register',
@@ -17,13 +18,44 @@ import { UiInputPassword } from '../../../../shared/ui/ui-input-password/ui-inpu
 })
 export class Register {
   private router = inject(Router);
+  private passwordValidator: ValidatorFn = (
+    control: AbstractControl<string | null>
+  ): ValidationErrors | null => {
+    const value = control.value;
+    if (!value) return null;
+
+    const errors: ValidationErrors = {};
+    if (/\s/.test(value)) errors['whitespace'] = true;
+    if (/[а-яА-ЯёЁ]/.test(value)) errors['cyrillic'] = true;
+    if (!/^[A-Za-z\d\W_]+$/.test(value)) errors['invalidCharacters'] = true;
+    if (!/[A-Z]/.test(value)) errors['uppercase'] = true;
+    if (!/[a-z]/.test(value)) errors['lowercase'] = true;
+    if (!/\d/.test(value)) errors['digit'] = true;
+    if (!/[^\w\s]/.test(value)) errors['specialCharacter'] = true;
+    if (value.length < 6) {
+      errors['minLength'] = {
+        requiredLength: 6,
+        actualLength: value.length,
+      };
+    }
+    return Object.keys(errors).length ? errors : null;
+  };
+  private phoneNumberMinLenValidator: ValidatorFn = (
+    control: AbstractControl<string | null>
+  ): ValidationErrors | null => {
+    const value = control.value;
+    if (!value) return null;
+    if (value.length < 12) return {minLength: true};
+    return null;
+  };
+
   protected form = new FormGroup({
     login: new FormControl<string | null>(null, {validators: [Validators.required]}),
-    password: new FormControl<string | null>(null, {validators: [Validators.required]}),
+    password: new FormControl<string | null>(null, {validators: [Validators.required, this.passwordValidator]}),
     lastName: new FormControl<string | null>(null, {validators: [Validators.required]}),
     firstName: new FormControl<string | null>(null, {validators: [Validators.required]}),
     patronymic: new FormControl<string | null>(null),
-    phone: new FormControl<string | null>(null, {validators: [Validators.required]}),
+    phone: new FormControl<string | null>(null, {validators: [Validators.required, this.phoneNumberMinLenValidator]}),
     email: new FormControl<string | null>(null, {validators: [Validators.required]}),
   });
 
